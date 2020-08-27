6b154b8efcb1a61c17f1f18671b735a539339f24
Guilherme Pereira Bertola <guilherme.bertola@ipeas.local>
2020-08-27 10:27:04 -0300

    Criação dos packs, view principal e classe de banco

---
+++ src/Connection/ConnectionFactory.java
@@ -0,0 +1,59 @@
+
+package connection;
+
+/*Importação das bibliotecas de Banco de dados*/
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+    // Dados para se conectar com o BD
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost:3306/";
+    private static final String USER = "java";
+    private static final String PASS = "password";
+    
+    //Metodo para iniciar a conexão com o Banco
+    public static Connection getConnection(){
+        try {
+            Class.forName(DRIVER);
+            return DriverManager.getConnection(URL, USER, PASS);
+        } catch (ClassNotFoundException | SQLException ex) {
+            throw new RuntimeException("ERRO NA CONEXÃO: ",ex);
+        }
+    }
+    // Metodo para fechar a conexão com o banco (genérico)
+    public static void closeConnection(Connection con){
+            try {
+                if (con != null){
+                con.close();
+                }
+            } catch (SQLException ex) {
+                throw new RuntimeException("ERRO AO FECHAR CONEXÃO: ",ex);
+            }
+    }
+    // Metodo para fechar a conexão com o banco (com o statement)
+    public static void closeConnection(Connection con, PreparedStatement stmt){
+            closeConnection(con);
+            try {
+               if (stmt != null){
+                stmt.close();
+               }
+            } catch (SQLException ex) {
+                throw new RuntimeException("ERRO AO FECHAR CONEXÃO: ",ex);
+            }
+    }
+    // Metodo para fechar a conexão com o banco (com o statement e o resultado)
+    public static void closeConnection(Connection con, PreparedStatement stmt, ResultSet rs){
+            closeConnection(con,stmt);
+            try {
+               if (rs != null){
+                   rs.close();
+               }
+            } catch (SQLException ex) {
+                throw new RuntimeException("ERRO AO FECHAR CONEXÃO: ",ex);
+            }
+    }
+}
